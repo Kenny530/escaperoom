90ce52c82989ee04df86349eef89e4b6e302be20
Kenny530 <kenneth_garcia@davincischools.org>
2019-03-11 13:12:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+// https://www.w3schools.com/jsref/met_document_getelementbyid.asp
+var timer = setInterval(checkGauges, 1000);
+var a = document.getElementById("left");
+var b = document.getElementById("right");
+var c = document.getElementById("button1");
+var key = document.getElementById("keybox");
+var sound = document.getelementbyId("keynoise");
+
+function nextImage(el){
+	if (a.src.match("images/gauge1.png")){
+		a.src = "images/gauge2.png";
+	} else if (a.src.match("images/gauge2.png")){
+		a.src = "images/gauge3.png";
+	} else if (a.src.match("images/gauge3.png")){
+		a.src = "images/gauge4.png";
+	}
+}
+function botImage(el){
+	if (a.src.match("images/gauge4.png")){
+		a.src = "images/gauge3.png";
+	} else if (a.src.match("images/gauge3.png")){
+		a.src = "images/gauge2.png";
+	} else if (a.src.match("images/gauge2.png")){
+		a.src = "images/gauge1.png";
+	} 
+}
+function nextImage2(el){
+	if (b.src.match("images/gauge1.png")){
+		b.src = "images/gauge2.png";
+	} else if (b.src.match("images/gauge2.png")){
+		b.src = "images/gauge3.png";
+	} else if (b.src.match("images/gauge3.png")){
+		b.src = "images/gauge4.png";
+	}
+}	
+function botImage2(el){
+	if (b.src.match("images/gauge4.png")){
+		b.src = "images/gauge3.png";
+	} else if (b.src.match("images/gauge3.png")){
+		b.src = "images/gauge2.png";
+	} else if (b.src.match("images/gauge2.png")){
+		b.src = "images/gauge1.png";
+	} 
+}
+
+function checkGauges(){
+	if (a.src.match("images/gauge4.png") && b.src.match("images/gauge2.png")){
+		key.src = "images/key.jpg";
+		//a.src = "images/spill.jpg";
+		//b.src = "images/spill.jpg";
+	} 
+}
+
+function unlOck(){
+	if (key.src.match("images/key.jpg")){
+		sounds.play();
+	}
+}
+function freEdom(){
+	if(c.src.match("images/door.png")){
+		window.location.href = "end.html";
+	}
+}
+		
+		
